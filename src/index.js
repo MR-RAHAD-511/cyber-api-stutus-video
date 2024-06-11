41685d2b229ae642c32e6b10e15c60a6cc1b3dd1
Mohammad Rahad <126962816+MR-RAHAD-511@users.noreply.github.com>
2024-06-12 03:57:49 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,19 +10,15 @@ const statusesData = JSON.parse(fs.readFileSync('status.json'));
 app.get('/status', (req, res) => {
   const randomIndex = Math.floor(Math.random() * statusesData.length);
   const randomStatus = statusesData[randomIndex];
-  const author = "Islamick Cyber Chat";
   const responseData = {
-    author,
-    url: {
-      title: randomStatus.title,
-      url: randomStatus.url,
-    },
+    title: randomStatus.title,
+    url: randomStatus.url,
+    VideoCount: randomStatus.videoCount 
   };
   console.log('Response:', responseData);
   res.json(responseData);
 });
 
-
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-}); 
+});
